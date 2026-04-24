ec820e61a2110ae0a3d978429fbaacd69bf80504
Handy Milliance <handymilliance.fikiria@gmail.com>
2026-04-24 00:30:32 -0300

    atualizado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,5 @@ export const environment = {
   //apiUrl: 'https://gerenciamento-tarefas-back-end.onrender.com',
   apiUrl: 'https://desafio-backend-java.onrender.com',
   googleClientId: '384430334755-i64ira308qkl8egqp0bb4k6i89fuf4tr.apps.googleusercontent.com',
-  //googleClientSecret: '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',
   adminEmail: 'milliance23@gmail.com'
 };
