4afb402a38b1653f92044afdfc5f6b6784af5a89
Handy Milliance <handymilliance.fikiria@gmail.com>
2026-04-25 00:30:54 -0300

    atualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DepartamentoPessoasComponent } from './components/departamento-pessoas/departamento-pessoas.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'admin/login', pathMatch: 'full' },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: 'usuario',
+    loadChildren: () => import('./usuario/usuario.module').then(m => m.UsuarioModule)
+  },
   { path: 'departamentos/:departamentoId/pessoas', component: DepartamentoPessoasComponent }
 ];
 
